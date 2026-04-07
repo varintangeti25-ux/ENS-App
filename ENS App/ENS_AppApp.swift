417253c295ff7ac0973dfab89aff47d7cc93ccc3
varintangeti25-ux <varintangeti25@gmail.com>
2026-04-07 09:21:34 -0500

    Initial Commit

---
+++ ENS App/ENS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ENS_AppApp.swift
+//  ENS App
+//
+//  Created by 64005899 on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ENS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ENS AppTests/ENS_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ENS_AppTests.swift
+//  ENS AppTests
+//
+//  Created by 64005899 on 4/7/26.
+//
+
+import Testing
+@testable import ENS_App
+
+struct ENS_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
