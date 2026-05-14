019d2881d8f72d16327b261be1b4b0df2641c42b
varintangeti25-ux <varintangeti25@gmail.com>
2026-05-14 09:24:32 -0500

    Add secrets template.

---
+++ ENS App/SecretsTemplate.swift
@@ -0,0 +1,19 @@
+//
+//  SecretsTemplate.swift
+//  ENS App
+//
+//  Created by 64005899 on 5/14/26.
+//
+// Duplicate this file and rename it Secrets.swift, then uncomment the bits below.
+// Fill in the strings to be returned with actual values.
+//
+
+//class ENSSecrets {
+//    public static func apiKey() -> String {
+//        return ""
+//    }
+//    
+//    public static func spreadsheetId() -> String {
+//        return ""
+//    }
+//}
